57b5e5eeb84213c2b0eb5e49649705c07ddd365c
zhuxiaomei <zhuxiaomei.meleny@bytedance.com>
2020-07-05 11:01:25 +0800

    删除无用注释

--- app/src/main/java/com/example/mynotebook/Utils.java
+++ app/src/main/java/com/example/mynotebook/Utils.java
@@ -13,28 +13,6 @@ import java.util.HashMap;
  * date:   2020/7/1
  */
 public class Utils {
-    // TODO: 2020/7/1 添加连续返回退出，用register的方式？
-    //连续返回退出
-//    private long exitTime = 0;
-//
-//    @Override
-//    public boolean onKeyDown(int keyCode, KeyEvent event) {
-//        if (keyCode == KeyEvent.KEYCODE_BACK) {
-//            exit();
-//            return false;
-//        }
-//        return super.onKeyDown(keyCode, event);
-//    }
-//
-//    private void exit() {
-//        if ((System.currentTimeMillis() - exitTime) > 2000) {
-//            Toast.makeText(getApplicationContext(),
-//                    "你还没有保存修改哦！点击“修改”保存\n", Toast.LENGTH_SHORT).show();
-//            exitTime = System.currentTimeMillis();
-//        } else {
-//            finish();
-//        }
-//    }
     private static HashMap<Activity, Long> ACTIVITY_LAST_BACK_MAP = new HashMap<>();
     private static long EXIT_TIME = 2000;
 
@@ -57,8 +35,6 @@ public class Utils {
             toast("再按一次退出");
         }
     }
-
-    // TODO: 2020/7/1 log整合
 
     public static void toast(String text) {
         Toast.makeText(BaseApplication.context, text, Toast.LENGTH_SHORT).show();
