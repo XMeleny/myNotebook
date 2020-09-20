41043ebdb3a183edf17b730ba4a42974133c2c8c
zhuxiaomei <zhuxiaomei.meleny@bytedance.com>
2020-09-20 21:57:09 +0800

    合并AddActivity 和 DetailActivity

--- app/src/main/java/com/example/mynotebook/activity/DetailActivity.java
+++ app/src/main/java/com/example/mynotebook/activity/DetailActivity.java
@@ -1,10 +1,12 @@
 package com.example.mynotebook.activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.mynotebook.R;
@@ -14,36 +16,111 @@ public class DetailActivity extends AppCompatActivity {
 
     private static final String TAG = "DetailActivity";
 
+    //bundle key and value
+    private static final String KEY_MODE = "mode";
+    private static final String VALUE_MODE_INSERT = "insert";
+    private static final String VALUE_MODE_UPDATE = "update";
+
+    private static final String KEY_ID = "id";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_CONTENT = "content";
+
+    //exception cases
+    private static final String EXCEPTION_CASE_INIT_ERROR = "init DetailActivity error, please use getXxxIntent to start activity";
+
+    //data
+    private String mode;
     private int id;
-    private EditText title;
-    private EditText content;
+
+    //ui
+    private EditText etTitle;
+    private EditText etContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        title = findViewById(R.id.et_title);
-        content = findViewById(R.id.et_content);
+        initView();
+        initData();
+    }
 
+    private void initView() {
+        etTitle = findViewById(R.id.et_title);
+        etContent = findViewById(R.id.et_content);
+    }
+
+    private void initData() {
         Intent intent = getIntent();
-        id = Integer.parseInt(intent.getStringExtra("id"));
-        title.setText(intent.getStringExtra("title"));
-        content.setText(intent.getStringExtra("content"));
+
+        mode = intent.getStringExtra(KEY_MODE);
+
+        if (TextUtils.isEmpty(mode)) {
+            throw new IllegalStateException(EXCEPTION_CASE_INIT_ERROR);
+        }
+
+        switch (mode) {
+            case VALUE_MODE_INSERT:
+                //do nothing
+                break;
+            case VALUE_MODE_UPDATE:
+                id = intent.getIntExtra("id", 0);
+                etTitle.setText(intent.getStringExtra("title"));
+                etContent.setText(intent.getStringExtra("content"));
+                break;
+            default:
+                break;
+        }
+    }
+
+    public static void insertMemo(@NonNull Context context) {
+        Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra(KEY_MODE, VALUE_MODE_INSERT);
+        context.startActivity(intent);
+    }
+
+    public static void updateMemo(@NonNull Context context, int id, String title, String content) {
+        Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra(KEY_MODE, VALUE_MODE_UPDATE);
+
+        intent.putExtra(KEY_ID, id);
+        intent.putExtra(KEY_TITLE, title);
+        intent.putExtra(KEY_CONTENT, content);
+
+        context.startActivity(intent);
     }
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
 
-        String strTitle = title.getText().toString().trim();
-        String strContent = content.getText().toString().trim();
+        String title = etTitle.getText().toString().trim();
+        String content = etContent.getText().toString().trim();
 
-        // 没有内容则删除
-        if (TextUtils.isEmpty(strTitle) && TextUtils.isEmpty(strContent)) {
-            NotebookDatabaseHelper.deleteById(id);
+        if (TextUtils.isEmpty(title) && TextUtils.isEmpty(content)) {
+            switch (mode) {
+                case VALUE_MODE_INSERT:
+                    //do nothing
+                    break;
+                case VALUE_MODE_UPDATE:
+                    NotebookDatabaseHelper.deleteById(id);
+                    break;
+                default:
+                    break;
+            }
         } else {
-            NotebookDatabaseHelper.update(id, strTitle, strContent);
+            switch (mode) {
+                case VALUE_MODE_INSERT:
+                    NotebookDatabaseHelper.insert(title, content);
+                    break;
+                case VALUE_MODE_UPDATE:
+                    NotebookDatabaseHelper.update(id, title, content);
+                    break;
+                default:
+                    break;
+            }
         }
     }
+
+
 }
